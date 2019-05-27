735f3bc9b0abdee8a5791890c46360320a6df50a
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-05-26 22:46:39 -0700

    Add seed file to start building db

---
+++ db_seed.py
@@ -0,0 +1,85 @@
+restaurants = [{
+    "name": "Le Comptoir",
+    "url": "https://www.lecomptoirla.com/"
+}, {
+    "name": "Perch",
+    "url": "https://perchla.com/"
+}, {
+    "name": "Bestia",
+    "url": "https://bestiala.com/"
+}, {
+    "name": "Majordomo",
+    "url": "https://www.majordomo.la/"
+}, {
+    "name": "Providence",
+    "url": "https://providencela.com/"
+}, {
+    "name": "Sushi Ginza Onodera",
+    "url": "https://onodera-group.com/sushi-la/"
+}, {
+    "name": "Spago",
+    "url": "https://wolfgangpuck.com/dining/spago-2/"
+}, {
+    "name": "CUT",
+    "url": "https://wolfgangpuck.com/dining/cut-beverly-hills/"
+}, {
+    "name": "El Cid",
+    "url": "https://www.elcidsunset.com/"
+}, {
+    "name": "Nobu",
+    "url": "https://www.noburestaurants.com/losangeles/home/"
+}, {
+    "name": "n/naka",
+    "url": "https://n-naka.com/"
+}, {
+    "name": "Trois Familia",
+    "url": "https://www.troisfamilia.com/"
+}, {
+    "name": "Catch",
+    "url": "https://catchrestaurants.com/catchla/"
+}, {
+    "name": "Jon and Vinny",
+    "url": "https://www.jonandvinnys.com/"
+}, {
+    "name": "Felix",
+    "url": "https://felixla.com/"
+}, {
+    "name": "Kismet",
+    "url": "https://www.kismetlosangeles.com/"
+}, {
+    "name": "Orsa and Winston",
+    "url": "http://www.orsaandwinston.com/home/"
+}, {
+    "name": "71above",
+    "url": "https://www.71above.com/"
+}, {
+    "name": "Bavel",
+    "url": "https://baveldtla.com/"
+}, {
+    "name": "Broken Spanish",
+    "url": "https://brokenspanish.com/"
+}, {
+    "name": "Otium",
+    "url": "https://otiumla.com/"
+}, {
+    "name": "The Exchange",
+    "url": "https://freehandhotels.com/los-angeles/the-exchange/"
+}, {
+    "name": "Best Girl",
+    "url": "https://www.bestgirldtla.com/"
+}, {
+    "name": "Maude",
+    "url": "https://www.mauderestaurant.com/"
+}, {
+    "name": "Mastro's Ocean Club",
+    "url": "https://www.mastrosrestaurants.com/"
+}, {
+    "name": "Chateau Marmont",
+    "url": "http://www.chateaumarmont.com/therestaurant.php"
+}, {
+    "name": "Gjelina",
+    "url": "https://www.gjelina.com/"
+}, {
+    "name": "Hatchet Hall",
+    "url": "https://www.hatchethallla.com/"
+}]
