d2af7b111a7bdadd2b179e066f8b998c841e9dab
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-05-28 09:39:44 -0700

    Add get route to show single user info

--- resources/users.py
+++ resources/users.py
@@ -1,13 +1,16 @@
 import json
 from flask import jsonify, Blueprint
-from flask_restful import (Resource, Api, reqparse, inputs, fields, marshal, marshal_with, url_for)
+from flask_restful import (Resource, Api, reqparse,
+                           inputs, fields, marshal, marshal_with, url_for)
 from flask_login import login_user, logout_user, login_required, current_user
 import models
 
 user_fields = {
     'id': fields.Integer,
+    'email': fields.String,
     'username': fields.String
 }
+
 
 class Register(Resource):
     def __init__(self):
@@ -48,6 +51,7 @@ class Register(Resource):
             'error': 'Passwords do not match'
         })
 
+
 class Login(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
@@ -71,10 +75,56 @@ class Login(Resource):
             login_user(user)
             return marshal(user, user_fields), 200
 
+
 class Logout(Resource):
     def post(self):
         logout_user()
         return 200
+
+
+class Single_User(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'id',
+            required=False,
+            help='No id provided',
+            location=['args']
+        )
+        self.reqparse.add_argument(
+            'password',
+            required=False,
+            help='No password provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'email',
+            required=False,
+            help='No email provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'username',
+            required=False,
+            help='No username provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'password',
+            required=False,
+            help='No password provided',
+            location=['form', 'json']
+        )
+
+    @marshal_with(user_fields)
+    def get(self, id):
+        try:
+            user = models.User.get_by_id(id)
+        except models.User.DoesNotExist:
+            raise Exception('There is no user with this ID')
+        else:
+            return (user, 200)
+
 
 users_api = Blueprint('resources.users', __name__)
 api = Api(users_api)
@@ -89,4 +139,8 @@ api.add_resource(
 api.add_resource(
     Logout,
     '/logout'
+)
+api.add_resource(
+    Single_User,
+    '/<int:id>'
 )
